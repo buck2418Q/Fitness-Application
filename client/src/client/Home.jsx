7a59a1306cd93b8ce2898a5a5792da20b4132519
KromaStone <groot707cod@gmail.com>
2024-11-02 18:26:30 +0530

    fixes

--- client/src/client/Home.jsx
+++ client/src/client/Home.jsx
@@ -1,6 +1,6 @@
 import '../styleSheets/Home.css'
 import React, { Suspense } from 'react'
-const Loader = React.lazy(() => import('../components/Button'))
+const Loader = React.lazy(() => import('../components/Loader.jsx'))
 const Banner = React.lazy(() => import('./home/Banner.jsx'));
 const StatBanner = React.lazy(() => import('./home/StatBanner.jsx'));
 const ChooseUs = React.lazy(() => import('./home/ChooseUs.jsx'));
@@ -12,7 +12,6 @@ const CallUs = React.lazy(() => import('./home/CallUs.jsx'));
 
 
 
-import LazyLoad from './../components/LazyLoad.jsx'
 
 function Home() {
   const components = [Banner, StatBanner, ChooseUs, Trainers, Coaches, JoinToday, Review, CallUs];
@@ -22,11 +21,11 @@ function Home() {
       <Suspense fallback={<div>
         <Loader />
       </div>}>
-        {components.map((Component, index) => (
-          <LazyLoad key={index}>
-            <Component />
-          </LazyLoad>
-        ))}
+
+        {Object.keys(components).map((key) => {
+          const Component = components[key];
+          return <Component key={key} />;
+        })}
 
         <section className='m-20'>
         </section>
