7190557663a63e541d2c010a2557680256740f96
KromaStone <groot707cod@gmail.com>
2024-12-07 14:34:14 +0530

    test

--- client/src/client/About.jsx
+++ client/src/client/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <div>
       <h1 className="text-red-800">about</h1>
       <button onClick={test} className='bg-green-200 px-4 py-2'>Test</button>
-
+      {/* test  */}
     </div>
   )
 }
