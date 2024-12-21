8cb2ab63689cef547fa037b3208155f78bc9b304
KromaStone <groot707cod@gmail.com>
2024-12-21 22:15:01 +0530

    K027

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,25 +16,26 @@ export default {
         background: "#111419",  // Main dark 
         secondary: "#1d2029",  // Secondary dark 
         light: "#ffffff",      // Light
-        secondlight:"#dddddd",
+        secondlight: "#dddddd",
         primary: "#16b650",    // Green 
         "hover-primary": "#13a048", // Slightly darker green for hover effects
       },
       backgroundImage: {
         'callbanner': "url('./src/assets/images/call-banner.png')",
         'footer-texture': "url('/img/footer-texture.png')",
-        'custom-gradient': 'linear-gradient(90deg, rgba(23,26,33,1) 33%, rgba(29,32,41,1) 100%)',
+        'custom-gradient': 'linear-gradient(90deg, rgba(255,255,255,1) 33%, rgba(255,255,255,0) 100%)',
       },
       dropShadow: {
         '3xl': '0 25px 25px rgba(0, 0, 0, 0.4)',
         '4xl': [
           '0 35px 35px rgba(0, 0, 0, 0.25)',
-          '0 45px 65px rgba(0, 0, 0, 0.15)',
+          '0 45px 65px rgba(0, 0, 0, 0.15)'
         ],
-        'myshadow': '0 45px 65px rgba(0,0,0,0.5)',
-      },
+        'myshadow': '0 45px 65px rgba(0,0,0,0.5)'
+      }
     },
   },
   darkMode: "class",
   plugins: [nextui()],
-};
+}
+
